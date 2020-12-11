8ef826d2beeb1f5fc0234d6a2e535f58e7d8ccce
aimee-liang <aimeeeliang@gmail.com>
2020-12-11 15:46:58 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Coding JS Problems
+![AltText](https://media.giphy.com/media/l2JehuRsc3JNiFhGo/giphy.gif)
+Practice Problems. Source: [CodingJS](https://the-winter.github.io/codingjs/)
---
+++ app.js
@@ -0,0 +1,10 @@
+/* 
+Warmup-1 -- endUp
+Given a string, return a new string where the last 3 chars are now in upper case. If the string has less than 3 chars, uppercase whatever is there. 
+Note that str.toUpperCase() returns the uppercase version of a string.
+
+Examples:
+    endUp('Hello') → HeLLO
+    endUp('hi there') → hi thERE
+    endUp('hi') → HI 
+*/

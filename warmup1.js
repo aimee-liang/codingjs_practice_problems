2fcaffdb8259100f079d392af2516198d8bd4bde
aimee-liang <aimeeeliang@gmail.com>
2020-12-12 17:01:13 -0500

    Renamed file structure for clarity, included new problem set

--- warmup1.js
+++ warmup1.js
@@ -29,3 +29,11 @@ function endUp(str){
     }
 }
 
+/* Warmup-1 -- sleepIn
+The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+
+Examples
+
+sleepIn(true, true) → true
+sleepIn(true, false) → false
+sleepIn(false, true) → true */

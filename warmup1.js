4db01375e259be232e8a57929906d5ccb8687e2c
aimee-liang <aimeeeliang@gmail.com>
2020-12-12 18:09:13 -0500

    Working on sleepIn()

--- warmup1.js
+++ warmup1.js
@@ -1,5 +1,4 @@
-/* 
-Warmup-1 -- endUp
+/* Warmup-1 -- endUp
 Given a string, return a new string where the last 3 chars are now in upper case. If the string has less than 3 chars, uppercase whatever is there. 
 Note that str.toUpperCase() returns the uppercase version of a string.
 
@@ -37,3 +36,16 @@ Examples
 sleepIn(true, true) → true
 sleepIn(true, false) → false
 sleepIn(false, true) → true */
+
+function sleepIn(weekday, vacation){
+    // weekday === true
+    // vacation === true
+    // if (!weekday) == false
+    if (weekday || vacation){
+        return true // if it is a weekday or we're on vacation
+    } else if (!weekday){
+        return true // if it's a weekend, we sleep in and it's true
+    } else if {
+        
+    }
+}

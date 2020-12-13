6cef1cd94ea55bbdba432fee36f1376a1c97fd41
aimee-liang <aimeeeliang@gmail.com>
2020-12-13 00:28:00 -0500

    Solved sumDouble()

--- warmup1.js
+++ warmup1.js
@@ -63,3 +63,20 @@ function monkeyTrouble(aSmile, bSmile){
         return false
     }
 }
+
+/* sumDouble
+Given two int values, return their sum. Unless the two values are the same, then return double their sum.
+
+Examples
+
+sumDouble(1, 2) → 3
+sumDouble(3, 2) → 5
+sumDouble(2, 2) → 8 */
+
+function sumDouble(a, b){
+    if (a === b){
+        return (a + b)*2
+    } else {
+        return a + b
+    }
+}

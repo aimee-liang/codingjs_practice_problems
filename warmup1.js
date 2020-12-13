3948410b3b5479831e3552b6fa53ef41500222d6
aimee-liang <aimeeeliang@gmail.com>
2020-12-13 00:31:05 -0500

    solved diff21()

--- warmup1.js
+++ warmup1.js
@@ -80,3 +80,20 @@ function sumDouble(a, b){
         return a + b
     }
 }
+
+/* diff21
+Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
+
+Examples
+
+diff21(19) → 2
+diff21(10) → 11
+diff21(21) → 0 */
+
+function diff21(n){
+    if (n <= 21){
+        return 21 - n
+    } else {
+        return (Math.abs(21-n) *2)
+    }
+}

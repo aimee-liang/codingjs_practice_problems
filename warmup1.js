e9bce870ea8f042c65c3d06b8444405b2710b9fd
aimee-liang <aimeeeliang@gmail.com>
2020-12-13 00:11:23 -0500

    Solved monkeyTrouble()

--- warmup1.js
+++ warmup1.js
@@ -47,3 +47,19 @@ function sleepIn(weekday, vacation){
     }
 }
 
+/* monkeyTrouble
+We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
+
+Examples
+
+monkeyTrouble(true, true) → true
+monkeyTrouble(false, false) → true
+monkeyTrouble(true, false) → false */
+
+function monkeyTrouble(aSmile, bSmile){
+    if (aSmile === bSmile){
+        return true
+    } else {
+        return false
+    }
+}

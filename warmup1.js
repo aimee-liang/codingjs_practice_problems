f0db559ef1c8fa8204e7818f7048159b66f7494d
aimee-liang <aimeeeliang@gmail.com>
2020-12-13 18:22:14 -0500

    Working on parrotTrouble()

--- warmup1.js
+++ warmup1.js
@@ -97,3 +97,22 @@ function diff21(n){
         return (Math.abs(21-n) *2)
     }
 }
+
+/* parrotTrouble
+We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
+
+Examples
+
+parrotTrouble(true, 6) → true
+parrotTrouble(true, 7) → false
+parrotTrouble(false, 6) → false */
+
+function parrotTrouble(talking, hour){
+    if (talking === true){
+        return true
+    } else if (hour < 7 && hour > 20) {
+        return true
+    } else {
+        return false
+    }
+}

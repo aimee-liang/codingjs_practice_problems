00fb181e2cb0c145577023e5cbf68c6b2029de1f
aimee-liang <aimeeeliang@gmail.com>
2020-12-14 17:36:03 -0500

    CRemoved examples from code

--- warmup1.js
+++ warmup1.js
@@ -30,12 +30,7 @@ function endUp(str){
 
 /* sleepIn
 The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
-
-Examples
-
-sleepIn(true, true) → true
-sleepIn(true, false) → false
-sleepIn(false, true) → true */
+ */
 
 function sleepIn(weekday, vacation){
     if (!weekday){
@@ -49,12 +44,7 @@ function sleepIn(weekday, vacation){
 
 /* monkeyTrouble
 We have two monkeys, a and b, and the parameters aSmile and bSmile indicate if each is smiling. We are in trouble if they are both smiling or if neither of them is smiling. Return true if we are in trouble.
-
-Examples
-
-monkeyTrouble(true, true) → true
-monkeyTrouble(false, false) → true
-monkeyTrouble(true, false) → false */
+ */
 
 function monkeyTrouble(aSmile, bSmile){
     if (aSmile === bSmile){
@@ -66,12 +56,7 @@ function monkeyTrouble(aSmile, bSmile){
 
 /* sumDouble
 Given two int values, return their sum. Unless the two values are the same, then return double their sum.
-
-Examples
-
-sumDouble(1, 2) → 3
-sumDouble(3, 2) → 5
-sumDouble(2, 2) → 8 */
+*/
 
 function sumDouble(a, b){
     if (a === b){
@@ -83,12 +68,7 @@ function sumDouble(a, b){
 
 /* diff21
 Given an int n, return the absolute difference between n and 21, except return double the absolute difference if n is over 21.
-
-Examples
-
-diff21(19) → 2
-diff21(10) → 11
-diff21(21) → 0 */
+*/
 
 function diff21(n){
     if (n <= 21){
@@ -100,12 +80,7 @@ function diff21(n){
 
 /* parrotTrouble
 We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23. We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true if we are in trouble.
-
-Examples
-
-parrotTrouble(true, 6) → true
-parrotTrouble(true, 7) → false
-parrotTrouble(false, 6) → false */
+ */
 
 function parrotTrouble(talking, hour){
     if (talking === true){
@@ -119,12 +94,7 @@ function parrotTrouble(talking, hour){
 
 /* makes10
 Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
-
-Examples
-
-makes10(9, 10) → true
-makes10(9, 9) → false
-makes10(1, 9) → true */
+ */
 
 function makes10(a, b){
     if (a === 10 || b === 10){
@@ -138,12 +108,7 @@ function makes10(a, b){
 
 /* nearHundred
 Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
-
-Examples
-
-nearHundred(93) → true
-nearHundred(90) → true
-nearHundred(89) → false */
+ */
 
 function nearHundred(n){
     if (Math.abs(n) < 90){
@@ -181,12 +146,7 @@ function posNeg(a, b, negative){
 
 /* notString
 Given a string, return a new string where "not " has been added to the front. However, if the string already begins with "not", return the string unchanged.
-
-Examples
-
-notString('candy') → not candy
-notString('x') → not x
-notString('not bad') → not bad */
+ */
 
 function notString(str){
     let subString = "not"
@@ -200,12 +160,7 @@ function notString(str){
 
 /* missingChar
 Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
-
-Examples
-
-missingChar('kitten', 1) → ktten
-missingChar('kitten', 0) → itten
-missingChar('kitten', 4) → kittn */
+ */
 
 function missingChar(str, n){
     return str.replace(str.charAt(n), "")
@@ -226,12 +181,7 @@ function frontBack(str){
 
 /* front3
 Given a string, we'll say that the front is the first 3 chars of the string. If the string length is less than 3, the front is whatever is there. Return a new string which is 3 copies of the front.
-
-Examples
-
-front3('Java') → JavJavJav
-front3('Chocolate') → ChoChoCho
-front3('abc') → abcabcabc */
+*/
 
 function front3(str){
     if (str.length <= 3){
@@ -244,12 +194,7 @@ function front3(str){
 
 /* or35
 Return true if the given non-negative number is a multiple of 3 or a multiple of 5. (Hint: Use the % 'mod' operator)
-
-Examples
-
-or35(3) → true
-or35(10) → true
-or35(8) → false */
+*/
 
 function or35(n){
     if (n % 3 === 0 || n % 5){
@@ -257,4 +202,17 @@ function or35(n){
     } else {
         return false
     }
+}
+
+/* startHi
+Given a string, return true if the string starts with 'hi' and false otherwise.
+
+Examples
+
+startHi('hi there') → true
+startHi('hi') → true
+startHi('hello hi') → false */
+
+function startHi(str){
+  
 }

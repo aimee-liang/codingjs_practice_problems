a14075817d400f7e94805f21992489646c97021c
aimee-liang <aimeeeliang@gmail.com>
2020-12-13 23:51:35 -0500

    Need to review parrotTrouble() and nearHundred()

--- warmup1.js
+++ warmup1.js
@@ -135,3 +135,16 @@ function makes10(a, b){
         return false
     }
 }
+
+/* nearHundred
+Given an int n, return true if it is within 10 of 100 or 200. Note: Math.abs(num) computes the absolute value of a number.
+
+Examples
+
+nearHundred(93) → true
+nearHundred(90) → true
+nearHundred(89) → false */
+
+function nearHundred(n){
+    if (Math.abs(n)
+}

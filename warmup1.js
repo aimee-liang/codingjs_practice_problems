cef9a9557ee2bb1b356b6c3093ce5195878c6c5b
aimee-liang <aimeeeliang@gmail.com>
2020-12-14 14:35:17 -0500

    Solved posNeg()

--- warmup1.js
+++ warmup1.js
@@ -156,3 +156,29 @@ function nearHundred(n){
         return true
     }
 }
+
+/* posNeg
+Given 2 int values, return true if one is negative and one is positive. Except if the parameter "negative" is true, then return true only if both are negative.
+
+Examples
+
+posNeg(1, -1, false) → true
+posNeg(-1, 1, false) → true
+posNeg(-4, -5, true) → true */
+
+/* if product is negative return true 
+if negative === true 
+*/
+
+function posNeg(a, b, negative){
+    let product = a * b
+    if (product < 0 && negative === false){
+        return true
+    } else if (a < 0 && b < 0 && negative === true){
+        return true
+    } else if (product > 0 && negative === false){
+        return false
+    } else {
+        return false
+    }
+}

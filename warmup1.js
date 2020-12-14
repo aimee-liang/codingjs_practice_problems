241398d2791528ba8bbddb3136ff563d485f72ff
aimee-liang <aimeeeliang@gmail.com>
2020-12-14 17:32:54 -0500

    Solved or35()

--- warmup1.js
+++ warmup1.js
@@ -241,3 +241,20 @@ function front3(str){
         return newStr+newStr+newStr
     }
 }
+
+/* or35
+Return true if the given non-negative number is a multiple of 3 or a multiple of 5. (Hint: Use the % 'mod' operator)
+
+Examples
+
+or35(3) → true
+or35(10) → true
+or35(8) → false */
+
+function or35(n){
+    if (n % 3 === 0 || n % 5){
+        return true
+    } else {
+        return false
+    }
+}

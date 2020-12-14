651ad831e4b766ead141ba2c8fcde192d31d1c12
aimee-liang <aimeeeliang@gmail.com>
2020-12-14 15:22:51 -0500

    Solved missingChar()

--- warmup1.js
+++ warmup1.js
@@ -197,3 +197,16 @@ function notString(str){
         return "not " + str
     }
 }
+
+/* missingChar
+Given a non-empty string and an int n, return a new string where the char at index n has been removed. The value of n will be a valid index of a char in the original string (i.e. n will be in the range 0..str.length()-1 inclusive).
+
+Examples
+
+missingChar('kitten', 1) → ktten
+missingChar('kitten', 0) → itten
+missingChar('kitten', 4) → kittn */
+
+function missingChar(str, n){
+    return str.replace(str.charAt(n), "")
+}

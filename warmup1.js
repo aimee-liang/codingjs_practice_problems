40d06eaa37feee26ecfec4ceef1fd170d5db5a2d
aimee-liang <aimeeeliang@gmail.com>
2020-12-16 19:11:10 -0500

    Working on delDel()

--- warmup1.js
+++ warmup1.js
@@ -370,3 +370,22 @@ function front22(str){
         return newStr+str+newStr
     }
 }
+
+/* delDel
+Given a string, if the string "del" appears starting at index 1, return a string where that "del" has been deleted. Otherwise, return the string unchanged.
+
+Examples
+
+delDel('adelbc') → abc
+delDel('adelHello') → aHello
+delDel('abcdel') → abcdel */
+
+function delDel(str){
+    if (str.length <= 3){
+        return str
+    } else if (str[1] != "d"){
+        return str
+    } else {
+        
+    }
+}

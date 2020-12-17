ec97dfe3c9c8509f7f666836d150514874e3bc46
aimee-liang <aimeeeliang@gmail.com>
2020-12-16 22:04:09 -0500

    delDel()

--- warmup1.js
+++ warmup1.js
@@ -371,7 +371,7 @@ function front22(str){
     }
 }
 
-/* delDel
+/* delDel --need to return to 
 Given a string, if the string "del" appears starting at index 1, return a string where that "del" has been deleted. Otherwise, return the string unchanged.
 
 Examples
@@ -383,9 +383,9 @@ delDel('abcdel') → abcdel */
 function delDel(str){
     if (str.length <= 3){
         return str
-    } else if (str[1] != "d"){
+    } else if (str[1] != "d" ){
         return str
     } else {
-        
+
     }
 }

b10ebc6790978d65f35bdfc40904fa5ae45bbb19
aimee-liang <aimeeeliang@gmail.com>
2020-12-17 23:37:45 -0500

    Solved sameFirstLast()

--- array1.js
+++ array1.js
@@ -5,3 +5,11 @@ function firstLast6(nums){
     if (nums[0] === 6 || nums[nums.length-1] === 6) return true
     return false
 }
+
+/* sameFirstLast
+Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal. */
+
+function sameFirstLast(nums){
+    if (nums.length >= 1 && nums[0] === nums[nums.length-1]) return true
+    return false
+}

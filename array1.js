6fc7dc24b2f57c30d68888ad6ba93d37df1f8d76
aimee-liang <aimeeeliang@gmail.com>
2020-12-19 20:35:47 -0500

    Completed makeEnds()

--- array1.js
+++ array1.js
@@ -66,3 +66,10 @@ function sum2(nums){
     return newSum.reduce((acc, cur) => acc+cur, 0)
     }
 }
+
+/* makeEnds
+Given an array of ints, return a new array length 2 containing the first and last elements from the original array. The original array will be length 1 or more. */
+
+function makeEnds(nums){
+    return [nums[0], nums[nums.length-1]]
+}

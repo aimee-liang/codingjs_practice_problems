5f5844d968d6d0b34684b5acb9627cce8370046d
aimee-liang <aimeeeliang@gmail.com>
2020-12-19 21:49:45 -0500

    Solved commonEnd()

--- array1.js
+++ array1.js
@@ -124,3 +124,11 @@ function frontPiece(nums){
     let newArr = nums.splice(0, 2)
     return newArr
 }
+
+/* commonEnd
+Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element. Both arrays will be length 1 or more. */
+
+function commonEnd(a, b){
+    if (a[0] === b[0] || a[a.length-1] === b[b.length-1]) return true
+    return false
+}

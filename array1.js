0119410cda3ab5ae19cdcab828164065addab01f
aimee-liang <aimeeeliang@gmail.com>
2020-12-19 20:59:40 -0500

    Solved double23()

--- array1.js
+++ array1.js
@@ -99,3 +99,12 @@ function no23(nums){
         }
     }
 }
+
+/* double23
+Given an int array, return true if the array contains 2 twice, or 3 twice. The array will be length 0, 1, or 2. */
+
+function double23(nums){
+    if (nums.length === 0 || nums.length === 1) return false
+    if (nums[0] === nums[nums.length-1]) return true
+    return false
+}

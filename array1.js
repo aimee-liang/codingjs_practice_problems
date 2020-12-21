9e1755303d050dfb12ea8845f919541521b40d6e
aimee-liang <aimeeeliang@gmail.com>
2020-12-20 22:12:39 -0500

    Completed fix23()

--- array1.js
+++ array1.js
@@ -166,3 +166,15 @@ function biggerTwo(a, b){
         return a
     }
 }
+
+/* fix23
+Given an int array length 3, if there is a 2 in the array immediately followed by a 3, set the 3 element to 0. Return the changed array. */
+
+function fix23(nums){
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] === 2 && nums[i+1] === 3){
+        nums[i+1] = 0
+        }
+    }
+    return nums
+}
